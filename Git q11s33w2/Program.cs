561bb40b0c0e4f6a1f0a12747d7c6bc60525ce2b
12PeirisI <12PeirisI@SMC-BITE2-06>
2024-07-11 09:44:57 +0100

    Add project files.

---
+++ Git q11s33w2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Git_q11s33w2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
